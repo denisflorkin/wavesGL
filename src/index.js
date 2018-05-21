31b9b5d4879047d7847c544ecc469d9b310b0f9c
Denis Florkin <denisflorkin@gmail.com>
2018-05-21 15:46:09 +0200

    save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 var camera, scene, renderer;
-var meshes
+var meshes = window.meshes = [];
 var geometries = window.geometries = [];
+var geometry = window.geometry = [];
 var GroupedVertice
 const existingIdenticalyLocatedVerticesModel = new Map();
-
-const makeWaves = (nWavesParam, xStartParam, yStartParam, intervalParam) => {
+let faces = []
+// const makeWaves = (nWavesParam, xStartParam, yStartParam, intervalParam) => {
+const makeVerticesCoords = (nWavesParam, xStartParam, yStartParam, intervalParam) => {
   // args:
   const nWaves = nWavesParam || 280 ; // triangles pair
   const cols = Math.ceil(Math.sqrt(nWaves / 2))
@@ -12,51 +14,63 @@ const makeWaves = (nWavesParam, xStartParam, yStartParam, intervalParam) => {
   const yStart = yStartParam || -(Math.ceil(cols / 2));
   const interval = intervalParam || .15
 
+  const verticesCoords = []
   const waves = []
   const xEnd = xStart + cols
   const yEnd = yStart + cols
+  const facesVertexRefs = []
 
   console.log(cols)
 
-  for (let i = yStart; i < yEnd; i++) {
+  // define vertices coords (v3)  [ x, y, z ]
+  for (let i = yStart; i <= yEnd; i++) {
     const row = i // y
-    for (let j = xStart; j < xEnd; j++) {
+    for (let j = xStart; j <= xEnd; j++) {
       const col = j // x
-
-      // supp:
-      waves.push([
-        [col * interval, -(row * interval), 0],
-        [(col + 1) * interval, -(row * interval), 0],
-        [col * interval, -((row + 1) * interval), 0],
-      ])
-
-      waves.push([
-        [(col + 1) * interval, -(row * interval), 0],
-        [(col + 1) * interval, -((row + 1) * interval), 0],
-        [col * interval, -((row + 1) * interval), 0],
-      ])
-
-      // inf
-      // waves.push([
-      //   [col * interval, row * interval, 0],
-      //   [(col + 1) * interval, row * interval, 0],
-      //   [row * interval, (row + 1) * interval, 0],
-      // ])
+      verticesCoords.push([col, row, 0])
+      // faces
+      // if (i < cols && j < cols) {
+      //   if (row === 0) {
+      //     faces.push([cols, cols + 1 , row + (cols+1) + 1])
+      //   } else if (row === 1) {
+      //     faces.push([(cols + row), (cols+row) + 1, (cols+row) + (cols + 1) + 1])
+      //   }
+      // }
     }
-    // inf:
-    // for (let j = 0; j < cols; j++) {
-    //   waves.push([
-    //     [i * interval, i * interval, 0],
-    //     [(i + 1) * interval, i * interval, 0],
-    //     [i * interval, (i + 1) * interval, 0],
-    //   ])
-    // }
   }
 
+  // faces = verticesCoords.map((vertex, i) => {
+  //   return [i, i + 1, i + 1 + (cols+1)]
+  // })
+
+  // define faces coords group   [  v3, v3, v3 ]
+  for (let i = 0; i < cols; i++) {
+    const row = i // y
+    for (let j = 0; j < cols; j++) {
+      const col = j + (i * cols) // x
+
+      // if (i < cols && j < cols) {
+        if (i === 0) {
+          faces.push([col, col + 1, col + 1 + (cols + 1)])
+          faces.push([(col+1), (col+2) + cols + 1, (col+1) + cols + 1])
+        } else if (i > 0) {
+          faces.push([(col + (row * cols)), (col + (row * cols)) + 1, (col + (row * cols)) + 1 + cols])
+          faces.push([
+            (col + (row * cols) + 1),
+            (col + (row * cols) + 2) + cols,
+            (col + (row * cols) + 1) + cols
+          ])
+        }
+      // }
+    }
+  }
+
+  return verticesCoords
   return waves
 }
 
-var theWaves = makeWaves(8, null, null, .1)
+// var theWaves = makeWaves(8, null, null, .1)
+var verticesRawCoordsData = makeVerticesCoords(8, null, null, .1)
 var waves = [ // vertices
   [
     [0, 0, 0],
@@ -78,14 +92,43 @@ var waves = [ // vertices
 ]
 
 console.log('TEMPALTE:', waves)
-console.log(theWaves)
+console.log('verticesRawCoordsData', verticesRawCoordsData)
+console.log('faces', faces)
 
 
-let colorIdx
-const getColor = () => {
-  const colors = [/* 0xffaa00, 0xcecece, */ 0xff000077, 0x0000ff77, 0x00ff0077 ]
-  const color = colors[colorIdx] ? colors[colorIdx++] : colors[colorIdx = 0]
-  return color
+let colorIdx = 0
+const getColor = (i) => {
+  const colors = [
+    "green",
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "rebeccapurple",
+  ]
+  const color = colors[colorIdx]
+
+  let res = colors[0]
+
+  return colors[0]
+
+  if (color) {
+    colorIdx = colorIdx + 1
+    console.log(color)
+    res = color
+  } else {
+    colorIdx = 0
+    console.log(color[0])
+    res = color[0]
+  }
+
+  res = colors[
+    colors[Math.floor(Math.random() * colors.length)]
+  ]
+
+  return res;
+  // return new THREE.Color(res);
+
 }
 
 const verticesStore = {}
@@ -94,86 +137,106 @@ const verticesDirectionStore = {}
 function init() {
   console.log('init')
   camera = new THREE.PerspectiveCamera(24, window.innerWidth / window.innerHeight, 0.01, 100);
+
   camera.position.z = 1;
 
   scene = new THREE.Scene();
   var group = new THREE.Group();
-
+  geometry = new THREE.Geometry();
 
 
   // meshes = waves.map((wave, i) => {
-  meshes = theWaves.map((wave, i) => {
-    const keyStrA = JSON.stringify(wave[0])
-    const keyStrB = JSON.stringify(wave[1])
-    const keyStrC = JSON.stringify(wave[2])
+  verticesRawCoordsData.forEach((v3, i) => {
+    // const keyStrA = JSON.stringify(wave[0])
+    // const keyStrB = JSON.stringify(wave[1])
+    // const keyStrC = JSON.stringify(wave[2])
 
-    let vertexA = new THREE.Vector3(...wave[0])
-    let vertexB = new THREE.Vector3(...wave[1])
-    let vertexC = new THREE.Vector3(...wave[2])
+    let vertex = new THREE.Vector3(...v3)
+    console.log(v3)
+    console.log(vertex)
+    // let vertexB = new THREE.Vector3(...wave[1])
+    // let vertexC = new THREE.Vector3(...wave[2])
 
-    if (verticesStore[keyStrA]) {
-      vertexA = verticesStore[keyStrA]
-    } else {
-      verticesStore[keyStrA] = vertexA
-      verticesDirectionStore[keyStrA] = Math.random() < .5
-    }
-    if (verticesStore[keyStrB]) {
-      vertexB = verticesStore[keyStrB]
-    } else {
-      verticesStore[keyStrB] = vertexB
-      verticesDirectionStore[keyStrA] = Math.random() < .5
-    }
-    if (verticesStore[keyStrC]) {
-      vertexC = verticesStore[keyStrC]
-    } else {
-      verticesStore[keyStrC] = vertexC
-      verticesDirectionStore[keyStrA] = Math.random() < .5
-    }
+    // if (verticesStore[keyStrA]) {
+    //   vertexA = verticesStore[keyStrA]
+    // } else {
+    //   verticesStore[keyStrA] = vertexA
+    //   verticesDirectionStore[keyStrA] = Math.random() < .5
+    // }
+    // if (verticesStore[keyStrB]) {
+    //   vertexB = verticesStore[keyStrB]
+    // } else {
+    //   verticesStore[keyStrB] = vertexB
+    //   verticesDirectionStore[keyStrA] = Math.random() < .5
+    // }
+    // if (verticesStore[keyStrC]) {
+    //   vertexC = verticesStore[keyStrC]
+    // } else {
+    //   verticesStore[keyStrC] = vertexC
+    //   verticesDirectionStore[keyStrA] = Math.random() < .5
+    // }
 
-    var geometry = new THREE.Geometry();
 
     geometry.vertices.push(
-      vertexA,
-      vertexB,
-      vertexC,
+      vertex,
     )
 
     for (let i = 0; i < geometry.vertices.length; i++) {
       geometry.vertices[i].___asc = (Math.random() > .5)
     }
 
-    var normal = new THREE.Vector3(0, 1, 0); //optional
-    var color = new THREE.Color(getColor(i)); //optional
-    var materialIndex = 0; //optional
-    var face = new THREE.Face3(0, 1, 2, normal, color, materialIndex);
+    // var normal = new THREE.Vector3(0, 1, 0); //optional
+    // var color = new THREE.Color(getColor(i)); //optional
+    // var materialIndex = 0; //optional
+    // var face = new THREE.Face3(0, 1, 2, normal, color, materialIndex);
 
-    geometry.faces.push(face);
+    // geometry.faces.push(face);
 
     const material = new THREE.MeshBasicMaterial({
-      color: getColor(i)
+      // color: getColor(i)
     });
     // material.color = getColor(i);
     material.side = THREE.DoubleSide;
     material.doubleSided = true;
+    material.vertexColor = THREE.FaceColors;
 
     const mesh = new THREE.Mesh(geometry, material);
     // mesh.position.x = (i * 5)
     group.add(mesh)
-    geometries.push(geometry)
+    // geometries.push(geometry)
     geometry.verticesNeedUpdate = true;
+    geometry.colorsNeedUpdate = true;
 
-    return mesh
+      // return mesh
   })
 
-  group.rotation.x = -20
+  faces.forEach((face, i) => {
+      var normal = new THREE.Vector3(0, 1, 0); //optional
+      var color = new THREE.Color(getColor(i)); //optional
+      var materialIndex = 0; //optional
+      var face = new THREE.Face3(...face, normal, color, materialIndex);
+    geometry.faces.push(
+      face
+    )
+  })
+
+  geometry.vertices.forEach((v3, i) => {
+    const vertex = geometry.vertices[i]
+
+  })
+
+
+
+  // group.rotation.x = -20
   // group.rotation.y = -.1
   // group.rotation.z = .3
   // group.position.x = -(window.innerWidth / 2);
   // group.position.y = .2;
+  group.position.z = -10;
   // meshes.forEach(geo => scene.add(geo))
   scene.add(group)
 
-  console.log('geometries', geometries)
+  console.log('geometry', geometry)
   console.log('verticesStore', verticesStore)
   console.log('verticesDirectionStore', verticesDirectionStore)
 
@@ -257,7 +320,7 @@ function init() {
     }
   }
 
-  console.log(existingIdenticalyLocatedVerticesModel)
+  // console.log(existingIdenticalyLocatedVerticesModel)
 
   // geometry.vertices.push(
   //   new THREE.Vector3(-.10, .10, 0),
@@ -294,7 +357,7 @@ function init() {
 
 function animate() {
 
-  requestAnimationFrame(animate);
+  // requestAnimationFrame(animate);
 
 
   let pointercount = 0
